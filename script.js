de95cdabbb780ad24cc7bc00c9bb734c1c84b96e
Bokashavadzee <105489089+Bokashavadzee@users.noreply.github.com>
2022-05-17 00:12:06 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// 1.შექმენით ფუნქცია, რომელსაც გადავცემთ n რაოდენობის მნიშვნელობას და დააბრუნოს მხოლოდ დადებითი რიცხვების ჯამს:
+
+// let array = [2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8];
+// function testFunction(...n){
+//     console.log(n);
+// }
+// testFunction(2 + 5 + 11 + 100 + 5 + 8);
+
+// 2. ფუნქციის საშულებით გამოიტანეთ ყველა რიცხვის ჯამი:
+// 10, 50, 6, 7, 8, 11, 6, 3, 9
+
+// function numbers (...arguments){
+//     let numberSum = 0;
+//     for ( let x of arguments){
+//         numberSum = numberSum + x;
+//     }
+//     console.log(numberSum);
+// }
+// numbers(10, 50, 6, 7, 8, 11, 6, 3, 9);
+
+// 3.
+// ფუნქციამ უნდა დააბრუნოს სახელი და გვარი ადგილის გამოტოვებით (მაგ: 'giorgi saakadze') თუ isloggedin არის true და false თუ isloggedin ფროფერთი არის false;
+
+
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'saakadze',
+//     age: 32,
+//     isloggedin: true
+//   }
+ 
+// function user2() {
+//   if(user.isloggedin==true){
+//     console.log(user.firstname+ " " + user.lastname);
+//   }
+//   if(user.isloggedin==false){
+//     console.log(false)
+//   }
+// }
+// user2();
+
+
+
+// მეოთხე გამიჭირდა ვერ გავაკეთე ბოდიში:((((
